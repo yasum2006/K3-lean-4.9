bc4bd1cfa7203174c6e4e6f79c224a5ec018de9b
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2020-08-28 14:54:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Phicomm K3/g' package/base-files/files/bin/config_generate
+git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
